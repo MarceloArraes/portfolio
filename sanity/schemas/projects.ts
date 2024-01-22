abe77df31136b8f6e19ad25963462424eb8f791a
Marcelo <marcelo.arraes@gmail.com>
2024-01-22 22:52:19 +1030

    feat: add projects schema

--- sanity/schemas/projects.ts
+++ sanity/schemas/projects.ts
@@ -1,3 +1,4 @@
+
 export default {
     name: 'project',
     type: 'document',
@@ -7,6 +8,36 @@ export default {
         name: 'name',
         type: 'string',
         title: 'Name'
+      },
+      {
+        name: 'slug',
+        type: 'slug',
+        title: 'Slug of your blog article',
+        options: {
+          source:'title',
+        }
+      },
+      {
+        name: 'titleImage',
+        type: 'image',
+        title: 'Title Image'
+      },
+      {
+        name: 'smallDescription',
+        type: 'text',
+        title: 'Small Description'
+      },
+      {
+        name: 'content',
+        type: 'array',
+        title: 'Content',
+        of: [
+          {
+            type: 'block'
+          }
+        ]
       }
     ]
   }
+
+  // edit
