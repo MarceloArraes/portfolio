30d7874428c4a79bad8d1ad625514fa434d77f55
Marcelo <marcelo.arraes@gmail.com>
2024-01-30 18:45:09 +1030

    feat: changed the schema to fit my projects json/schema

---
+++ app/components/projectInfos.ts
@@ -0,0 +1,179 @@
+const mainProjects = [
+    {
+      name: 'GeoCapital stock Tracker',
+      description: 'A stock tracker for GeoCapital',
+      site: 'https://geo-capital-online-portfolio.vercel.app/',
+      src: '/GeoCapitalTest.png',
+      darkimage: true,
+      siteIcon: '/nutriNotes.png',
+  
+      active: true,
+      tecDescription: 'Typescript, Nextjs, Tailwindcss',
+      tecIcon1: '/typescripticon.png',
+      tecIcon2: '/nextjsicon.png',
+      tecIcon3: '/tailwindcssLogo.png',
+    },
+    {
+      name: 'Nutri Notes',
+      description: 'Nutritional information for health professionals',
+      site: 'https://nutri-notes.vercel.app/',
+      src: '/nutrinotesbg.png',
+      darkimage: false,
+  
+      active: true,
+      siteIcon: '/nutriNotes.png',
+      tecDescription: 'Nextjs, Supabase, MaterialUi',
+      tecIcon1: '/nextjsicon.png',
+      tecIcon2: '/supabaseicon.png',
+      tecIcon3: '/muiicon.png',
+    },
+    {
+      name: 'Web Widget',
+      description: 'A web widget for the web',
+      site: 'https://vercel.com/marceloarraes/web-widget',
+      src: '/webwidget_gif.gif',
+      darkimage: true,
+      siteIcon: '/webwidgetIcon.png',
+  
+      active: false,
+      tecDescription: 'Vite, Typescript, Tailwindcss',
+      tecIcon1: '/vitelogo.svg',
+      tecIcon2: '/typescripticon.png',
+      tecIcon3: '/tailwindcssLogo.png',
+    },
+  ]
+  
+  const secondProjects = [
+    {
+      name: 'Bem Paggo Form',
+      description: 'A front-end site for Bem Paggo company',
+      site: 'https://bem-pago-form.vercel.app/',
+      src: '/bempagobg.png',
+      darkimage: false,
+      siteIcon: '/bempaggoicon2.png',
+  
+      active: true,
+      tecDescription: 'Typescript, MaterialUI, Styled Components',
+      tecIcon1: '/typescripticon.png',
+      tecIcon2: '/muiicon.png',
+      tecIcon3: '/styledcomponentsicon3.png',
+    },
+    {
+      name: 'Event Platform',
+      description: 'A platform to display a online event',
+      site: 'https://event-platform-etdp6plb8-marceloarraes.vercel.app/',
+      src: '/eventplatform.png',
+      darkimage: true,
+      siteIcon: '/adamintericon.svg',
+  
+      active: true,
+      tecDescription: 'Reactjs, Vite, Graphql, vime, tailwindcss',
+      tecIcon1: '/reacticon.png',
+      tecIcon2: '/typescripticon.png',
+      tecIcon3: '/tailwindcssLogo.png',
+    },
+    {
+      name: 'My Portfolio',
+      description: 'My portfolio with some projects.',
+      site: 'https://marcelosportfolio.vercel.app/',
+      src: '/marcelosPortfolioImage.png',
+      darkimage: true,
+  
+      active: true,
+      siteIcon: '/nutriNotes.png',
+      tecDescription: 'Typescript, Nextjs, Tailwindcss',
+      tecIcon1: '/typescripticon.png',
+      tecIcon2: '/nextjsicon.png',
+      tecIcon3: '/tailwindcssLogo.png',
+    },
+  ]
+  
+  const oldProjects = [
+    {
+      name: 'Nasa Project',
+      description: 'Space mission scheduler',
+      site: 'https://gold-expensive-bream.cyclic.app/',
+      src: '/nasaProjectBack.png',
+      darkimage: true,
+      siteIcon: '/adamintericon.svg',
+  
+      active: false,
+      tecDescription: 'Nodejs, Reactjs, MongoDb',
+      tecIcon1: '/nodejsicon.png',
+      tecIcon2: '/reacticon.png',
+      tecIcon3: '/nodejsicon.png',
+    },
+    {
+      name: 'Adaminter.org',
+      description: 'Nft Creator on Cardano blockchain',
+      site: 'https://www.adaminter.org',
+      src: '/adaminterback.png',
+      darkimage: false,
+      siteIcon: '/adamintericon.svg',
+  
+      active: false,
+      tecDescription: 'Blockchain, Nextjs, Django, Nodejs',
+      tecIcon1: '/reacticon.png',
+      tecIcon2: '/djangoicon.png',
+      tecIcon3: '/nodejsicon.png',
+    },
+  ]
+  const mobileProjects = [
+    {
+      name: 'App Helper',
+      description: 'A tickering platform for technical support',
+      site: '',
+      src: '/144203-720x1604_AdobeExpress.gif',
+      darkimage: true,
+      siteIcon: '',
+  
+      active: false,
+      tecDescription: 'React-Native, native-base, MongoDB, firestore',
+      tecIcon1: '/reacticon.png',
+      tecIcon2: '/typescripticon.png',
+      tecIcon3: '/tailwindcssLogo.png',
+    },
+    {
+      name: 'Time Capsule',
+      description: 'A app to save your memories',
+      site: '',
+      src: '/timeCapsule.gif',
+      darkimage: true,
+      siteIcon: '',
+  
+      active: false,
+      tecDescription: 'React-Native, nativewind, prisma and expo-router',
+      tecIcon1: '/reacticon.png',
+      tecIcon2: '/typescripticon.png',
+      tecIcon3: '/tailwindcssLogo.png',
+    },
+    // {
+    //   name: 'App Helper',
+    //   description: 'A tickering platform for technical support',
+    //   site: '',
+    //   src: '/144203-720x1604_AdobeExpress.gif',
+    //   darkimage: true,
+    //   siteIcon: '',
+  
+    //   active: false,
+    //   tecDescription: 'React-Native, native-base, MongoDB, firestore',
+    //   tecIcon1: '/reacticon.png',
+    //   tecIcon2: '/typescripticon.png',
+    //   tecIcon3: '/tailwindcssLogo.png',
+    // },
+    // {
+    //   name: 'App Helper',
+    //   description: 'A tickering platform for technical support',
+    //   site: '',
+    //   src: '/144203-720x1604_AdobeExpress.gif',
+    //   darkimage: true,
+    //   siteIcon: '',
+  
+    //   active: false,
+    //   tecDescription: 'React-Native, native-base, MongoDB, firestore',
+    //   tecIcon1: '/reacticon.png',
+    //   tecIcon2: '/typescripticon.png',
+    //   tecIcon3: '/tailwindcssLogo.png',
+    // },
+  ]
+  
--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -5,3 +5,20 @@ export interface simpleProjectCard {
     titleImage: { _type:string, asset: [Object] };
     content?: any;
 }
+
+/*     
+{
+    name: 'GeoCapital stock Tracker',
+    description: 'A stock tracker for GeoCapital',
+    site: 'https://geo-capital-online-portfolio.vercel.app/',
+    src: '/GeoCapitalTest.png',
+    siteIcon: '/nutriNotes.png',
+    darkimage: true,
+
+    active: true,
+    tecDescription: 'Typescript, Nextjs, Tailwindcss',
+    tecIcon1: '/typescripticon.png',
+    tecIcon2: '/nextjsicon.png',
+    tecIcon3: '/tailwindcssLogo.png',
+},
+*/
